59d32b85c3d8194f6e839c9eb17f4e48e6c85650
RishiJhawar <88778724+RishiJhawar@users.noreply.github.com>
2021-08-11 17:37:46 +0530

    Create 120ME1010_Q1.cpp
    Solution to Question 3

---
+++ submissions/120ME1010/120ME1010_Q1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int i,mp;
+        int profit=0;
+        mp=prices[0];
+        for(i=0; i<prices.size(); i++)
+        {
+            if(mp>prices[i])
+            {
+                mp=prices[i];
+            }
+            if(prices[i]-mp>profit)
+            {
+                profit=prices[i]-mp;
+            }
+        }
+        return profit;
+    }
+};
